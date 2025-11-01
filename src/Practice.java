aae1b3b008a990463844a29856197a690f0ed3e7
anthonykravchishin <tonykravc53@gmail.com>
2025-11-01 14:11:54 -0700

    Added solutions and tests for maxDiff

--- src/Practice.java
+++ src/Practice.java
@@ -1,4 +1,8 @@
+import java.util.*;
+
 public class Practice {
+    // --- maxDiff ---
+
     /**
      * Returns the difference between the largest and smallest integer in an array.
      * 
@@ -7,13 +11,127 @@ public class Practice {
      */
     public static int maxDiff(int[] nums) {
         // TODO: implement this
-        return -1;
+        Arrays.sort(nums);
+        return nums[nums.length - 1] - nums[0];
     }
-
 
     // TODO: Implement the other methods from the study guide AND tests for each one
 
     // For each method you are only required to implement it for one of the data
     // structures. But use a different data structure for each method. For example,
     // do maxDiff with an array, the next question with a Set, etc.
+
+    /**
+     * Returns the difference between the largest and smallest integer in an
+     * arrayList.
+     * 
+     * @param nums a non-empty, non-null arrayList of numbers
+     * @return the difference between the largest and smallest number
+     */
+    public static int maxDiffArrayList(ArrayList<Integer> nums) {
+        // TODO: implement this
+        Collections.sort(nums);
+        return nums.get(nums.size() - 1) - nums.get(0);
+    }
+
+    /**
+     * Returns the difference between the largest and smallest integer in an
+     * HashSet.
+     * 
+     * @param nums a non-empty, non-null HashSet of numbers
+     * @return the difference between the largest and smallest number
+     */
+    public static int maxDiffHashSet(HashSet<Integer> nums) {
+        // TODO: implement this
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int num : nums) {
+            if (num < min) {
+                min = num;
+            }
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max - min;
+    }
+
+    /**
+     * Returns the difference between the largest and smallest keys in an
+     * HashMap.
+     * 
+     * @param nums a non-empty, non-null HashMap of numbers
+     * @return the difference between the largest and smallest keys
+     */
+    public static int maxDiffMapKeys(Map<Integer, Integer> nums) {
+        // TODO: implement this
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int num : nums.keySet()) {
+            if (num < min) {
+                min = num;
+            }
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max - min;
+    }
+
+    /**
+     * Returns the difference between the largest and smallest values in an
+     * HashMap.
+     * 
+     * @param nums a non-empty, non-null HashMap of numbers
+     * @return the difference between the largest and smallest values
+     */
+    public static int maxDiffMapValues(Map<Integer, Integer> nums) {
+        // TODO: implement this
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int num : nums.values()) {
+            if (num < min) {
+                min = num;
+            }
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max - min;
+    }
+
+    // --- longestWord ---
+
+    /**
+     * Returns the longest word that starts with a different character in an array.
+     * 
+     * @param words a non-empty, non-null, all-lowercase array of words
+     * @return the longest word that starts with c
+     */
+    public static String longestWord(String[] words, char c) {
+        String longest = "";
+        for (String word : words) {
+            if (word.startsWith(Character.toString(c)) && word.length() > longest.length()) {
+                longest = word;
+            }
+        }
+        return longest;
+    }
+
+    /**
+     * Returns the longest word that starts with a different character in an
+     * arrayList.
+     * 
+     * @param words a non-empty, non-null, all-lowercase arrayList of words
+     * @return the longest word that starts with c
+     */
+    public static String longestWordArrayList(ArrayList<String> words, char c) {
+        String longest = "";
+        for (String word : words) {
+            if (word.startsWith(Character.toString(c)) && word.length() > longest.length()) {
+                longest = word;
+            }
+        }
+        return longest;
+    }
 }
--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -2,13 +2,16 @@ import static org.junit.Assert.*;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.*;
+
 public class PracticeTest {
+    // --- maxDiff ---
 
     // TODO: Make more tests for maxDiff
     @Test
     void testMaxDiffPositiveAndNegative() {
         // Arrange
-        int[] numbers = {8, -2, 9, -5};
+        int[] numbers = { 8, -2, 9, -5 };
 
         // Act
         int actual = Practice.maxDiff(numbers);
@@ -17,10 +20,197 @@ public class PracticeTest {
         // Largest: 9, Smallest: -5, Difference: 9 - -5 = 14
         assertEquals(14, actual);
     }
-    
+
+    @Test
+    void testMaxDiffDuplicates() {
+        // Arrange
+        int[] numbers = { 3, 3, 3 };
+
+        // Act
+        int actual = Practice.maxDiff(numbers);
+
+        // Assert
+        // Largest: 3, Smallest: 3, Difference: 3 - 3 = 0
+        assertEquals(0, actual);
+    }
 
     // TODO: Make tests for each problem you solve
-    
+    @Test
+    void testMaxDiffArrayListPositiveAndNegative() {
+        // Arrange
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(8);
+        numbers.add(-2);
+        numbers.add(9);
+        numbers.add(-5);
+
+        // Act
+        int actual = Practice.maxDiffArrayList(numbers);
+
+        // Assert
+        // Largest: 9, Smallest: -5, Difference: 9 - -5 = 14
+        assertEquals(14, actual);
+    }
+
+    @Test
+    void testMaxDiffArrayListDuplicates() {
+        // Arrange
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(3);
+        numbers.add(3);
+
+        // Act
+        int actual = Practice.maxDiffArrayList(numbers);
+
+        // Assert
+        // Largest: 3, Smallest: 3, Difference: 3 - 3 = 0
+        assertEquals(0, actual);
+    }
+
+    @Test
+    void testMaxDiffHashSetPositiveAndNegative() {
+        // Arrange
+        HashSet<Integer> numbers = new HashSet<>();
+        numbers.add(8);
+        numbers.add(-2);
+        numbers.add(9);
+        numbers.add(-5);
+
+        // Act
+        int actual = Practice.maxDiffHashSet(numbers);
+
+        // Assert
+        // Largest: 9, Smallest: -5, Difference: 9 - -5 = 14
+        assertEquals(14, actual);
+    }
+
+    @Test
+    void testMaxDiffHashSetDuplicates() {
+        // Arrange
+        HashSet<Integer> numbers = new HashSet<>();
+        numbers.add(3);
+        numbers.add(3);
+        numbers.add(3);
+
+        // Act
+        int actual = Practice.maxDiffHashSet(numbers);
+
+        // Assert
+        // Largest: 3, Smallest: 3, Difference: 3 - 3 = 0
+        assertEquals(0, actual);
+    }
+
+    @Test
+    void testMaxDiffMapKeysPositiveAndNegative() {
+        // Arrange
+        Map<Integer, Integer> numbers = new HashMap<>();
+        numbers.put(8, -5);
+        numbers.put(-2, 9);
+        numbers.put(9, -2);
+        numbers.put(-5, 8);
+
+        // Act
+        int actual = Practice.maxDiffMapKeys(numbers);
+
+        // Assert
+        // Largest: 9, Smallest: -5, Difference: 9 - -5 = 14
+        assertEquals(14, actual);
+    }
+
+    @Test
+    void testMaxDiffMapKeysDuplicates() {
+        // Arrange
+        Map<Integer, Integer> numbers = new HashMap<>();
+        numbers.put(3, -2);
+        numbers.put(3, -2);
+        numbers.put(3, -2);
+
+        // Act
+        int actual = Practice.maxDiffMapKeys(numbers);
+
+        // Assert
+        // Largest: 3, Smallest: 3, Difference: 3 - 3 = 0
+        assertEquals(0, actual);
+    }
+
+    @Test
+    void testMaxDiffMapValuesPositiveAndNegative() {
+        // Arrange
+        Map<Integer, Integer> numbers = new HashMap<>();
+        numbers.put(8, -5);
+        numbers.put(-2, 9);
+        numbers.put(9, -2);
+        numbers.put(-5, 8);
+
+        // Act
+        int actual = Practice.maxDiffMapValues(numbers);
+
+        // Assert
+        // Largest: 9, Smallest: -5, Difference: 9 - -5 = 14
+        assertEquals(14, actual);
+    }
+
+    @Test
+    void testMaxDiffMapValuesDuplicates() {
+        // Arrange
+        Map<Integer, Integer> numbers = new HashMap<>();
+        numbers.put(3, -2);
+        numbers.put(3, -2);
+        numbers.put(3, -2);
+
+        // Act
+        int actual = Practice.maxDiffMapValues(numbers);
+
+        // Assert
+        // Largest: -2, Smallest: -2, Difference: -2 - -2 = 0
+        assertEquals(0, actual);
+    }
+
+    // --- longestWord ---
+
+    @Test
+    void testLongestWordOneMatch() {
+        String[] list = { "fire", "twenty", "lamp", "oxygen" };
+
+        String actual = Practice.longestWord(list, 'l');
+
+        assertEquals("lamp", actual);
+    }
+
+    @Test
+    void testLongestWordMultiMatch() {
+        String[] list = { "tire", "twenty", "tape", "titanium", "refrigerator" };
+
+        String actual = Practice.longestWord(list, 't');
+
+        assertEquals("titanium", actual);
+    }
+
+    @Test
+    void testLongestWordArrayListOneMatch() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("fire");
+        list.add("twenty");
+        list.add("lamp");
+        list.add("oxygen");
+
+        String actual = Practice.longestWordArrayList(list, 'l');
+
+        assertEquals("lamp", actual);
+    }
+
+    @Test
+    void testLongestWordArrayListMultiMatch() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("tire");
+        list.add("twenty");
+        list.add("tape");
+        list.add("titanium");
+        list.add("refrigerator");
+
+        String actual = Practice.longestWordArrayList(list, 't');
+
+        assertEquals("titanium", actual);
+    }
 }
-
-
